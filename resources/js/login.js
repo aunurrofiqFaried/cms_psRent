01c12fe33c8c00c47c56f5cdce5fd86c857ad716
aunurrofiqFaried <aunurrofiqWajdi@gmail.com>
2024-08-11 20:31:27 +0700

    fitur forget password

--- resources/js/login.js
+++ resources/js/login.js
@@ -24,6 +24,35 @@ document.getElementById('loginForm').addEventListener('submit', function(event) 
     .catch(function (error) {
         console.error('Login failed');
         console.error(error);
-        document.getElementById('errorMessage').innerText = 'Login Gagal!!';
+        document.getElementById('errorMessage').innerText = 'Failed Login!';
+        document.getElementById('forgetMessage').innerText = 'Forget Password!';
+    });
+});
+
+document.getElementById('chagePassForm').addEventListener('submit', function(event) {
+    event.preventDefault();
+
+    // Get user input
+    const email = document.getElementById('Email').value;
+    const password = document.getElementById('Password').value;
+    const repassword = document.getElementById('repassword').value;
+
+    if (password !== repassword) {
+        document.getElementById("Message").innerText = "Passwords do not match!";
+        return;
+    }
+    // Send POST request to API
+    axios.put('https://rentconsoleapi.yudho.online/other/password', {
+        email: email,
+        password: password
+    })
+    .then((response) => {
+        console.log(response)
+        document.getElementById("Message").innerText = "Password changed successfully!";
+        document.getElementById("Message").style.color = 'blue';
+    })
+    .catch((error) => {
+        console.error(error);
+        document.getElementById("Message").innerText = "Failed to change password!";
     });
 });
